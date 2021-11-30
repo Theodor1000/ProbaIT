5a6dfc3377790d2827a8e53021e2752ff30db69a
Theodor Cristea 322CAb <stefcrs@gmail.com>
2021-11-30 05:39:24 +0200

    get and post with validators

--- routes/conact_requests_route.js
+++ routes/conact_requests_route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {body, validationResult} = require('express-validator');
 const {getContactRequests, addContactRequest} = require('../database/contact_requests');
 
 const router = express.Router();
@@ -8,9 +9,40 @@ router.get('/', async (req, res) => {
     res.status(200).send(result);
 });
 
-router.post('/', async (req, res) => {
-    await addContactRequest(req.body);
-    res.status(200);
+router.post(
+    '/',
+    body('name')
+        .exists().withMessage('name must be provided')
+        .bail()
+        .isString().withMessage('name must be a string')
+        .bail()
+        .isLength({min: 1, max: 50}).withMessage('name must be between 1 and 50 characters long'),
+    body('email')
+        .exists().withMessage('email must be provided')
+        .bail()
+        .isEmail().withMessage('email is not valid')
+        .bail()
+        .isLength({min: 1, max: 50}).withMessage('email must be between 1 and 50 characters long'),
+    body('message')
+        .exists().withMessage('message must be provided')
+        .bail()
+        .isString().withMessage('message must be a string')
+        .bail()
+        .isLength({min: 1, max: 5000}).withMessage('message must be between 1 and 5000 characters long'),
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.status(400).send(formatErrorMessage(errors));
+            return;
+        }
+
+        await addContactRequest(req.body);
+        res.status(200).send();
 });
+
+function formatErrorMessage(errors) {
+    return errors.array().reduce((soFar, error) => soFar.concat(error.msg + '\n'), '');
+}
 
 module.exports = router;
