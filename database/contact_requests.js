f3eccec76cf55ae32d30051d489d6deed29bf1d9
Theodor Cristea 322CAb <stefcrs@gmail.com>
2021-11-30 06:17:03 +0200

    first task without bonus

--- database/contact_requests.js
+++ database/contact_requests.js
@@ -29,6 +29,7 @@ async function initializeContactRequestsTable(sequelize) {
     }, {
         sequelize,
         modelName: 'ContactRequest',
+        timestamps: false,
     });
     await ContactRequest.sync();
 }
@@ -37,12 +38,40 @@ async function getContactRequests() {
     const contactRequests = await ContactRequest.findAll();
     return JSON.stringify(contactRequests, null, 2);
 }
+
+async function getOneContactRequest(id) {
+    const contactRequests = await ContactRequest.findAll({where: {id}});
+    if (contactRequests.length === 0) {
+        return undefined;
+    }
+    return JSON.stringify(contactRequests, null, 2);
+}
+
 async function addContactRequest(params) {
-    await ContactRequest.create({
+    const result = await ContactRequest.create({
         name: params.name,
         email: params.email,
         message: params.message
     });
+    return JSON.stringify(result, null, 2);
 }
 
-module.exports = {initializeContactRequestsTable, getContactRequests, addContactRequest};
+async function updateContactRequest(id, is_resolved) {
+    const exists = await getOneContactRequest(id);
+    if (exists === undefined) {
+        return undefined;
+    }
+    const result = await ContactRequest.update({is_resolved}, {where: {id}});
+    return JSON.stringify(result, null, 2);
+}
+
+async function deleteContactRequest(id) {
+    const exists = await getOneContactRequest(id);
+    if (exists === undefined) {
+        return undefined;
+    }
+    const result = await ContactRequest.destroy({where: {id}});
+    return JSON.stringify(result, null, 2);
+}
+
+module.exports = {initializeContactRequestsTable, getContactRequests, addContactRequest, getOneContactRequest, updateContactRequest, deleteContactRequest};
--- routes/conact_requests_route.js
+++ routes/conact_requests_route.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const {body, validationResult} = require('express-validator');
-const {getContactRequests, addContactRequest} = require('../database/contact_requests');
+const {getContactRequests, addContactRequest, getOneContactRequest, updateContactRequest, deleteContactRequest} = require('../database/contact_requests');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     const result = await getContactRequests();
+    res.status(200).send(result);
+});
+
+router.get('/:id', async (req, res) => {
+    const result = await getOneContactRequest(req.params.id);
+    if (result === undefined) {
+        res.status(404).send('id not found');
+        return;
+    }
     res.status(200).send(result);
 });
 
@@ -37,9 +46,50 @@ router.post(
             return;
         }
 
-        await addContactRequest(req.body);
-        res.status(200).send();
+        const result = await addContactRequest(req.body);
+        res.status(200).send(result);
 });
+
+router.patch(
+    '/:id',
+    body('is_resolved')
+        .exists().withMessage('is_resolved must be provided')
+        .bail()
+        .isBoolean().withMessage('is_resolved must be a boolean'),
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.status(400).send(formatErrorMessage(errors));
+            return;
+        }
+        // 0 and 1 are accepted by the validator, so they are eliminated manually
+        if (req.body.is_resolved === 0 || req.body.is_resolved === 1) {
+            res.status(400).send('is_resolved must be a boolean');
+        }
+
+        const result = await updateContactRequest(req.params.id, req.body.is_resolved);
+
+        if (result === undefined) {
+            res.status(404).send('id not found');
+            return;
+        }
+
+        res.status(200).send('Success!');
+    });
+
+router.delete(
+    '/:id',
+    async (req, res) => {
+        const result = await deleteContactRequest(req.params.id);
+
+        if (result === undefined) {
+            res.status(404).send('id not found');
+            return;
+        }
+
+        res.status(200).send('Success!');
+    });
 
 function formatErrorMessage(errors) {
     return errors.array().reduce((soFar, error) => soFar.concat(error.msg + '\n'), '');
